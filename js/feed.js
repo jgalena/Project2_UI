6787dfdc167a2babcdf20657224042b46b2f61aa
KrolPower <osborne_chris@icloud.com>
2022-06-01 17:36:51 -0700

    removed unneeded file

--- js/feed.js
+++ js/feed.js
@@ -1,4 +1,3 @@
-//get all posts
 async function getPosts(){
   try{
     const raw_response = await fetch(
@@ -12,33 +11,29 @@ async function getPosts(){
       }
     );
 
-
     if (!raw_response.ok) {
       throw new Error(raw_response.status);
     }
 
     const json_data = await raw_response.json();
 
+    // post_id = json_data[0].post_id;
+    // post_body = json_data[0].post_body;
+    // post_likes = json_data[0].post_likes; 
+    // user = json_data[0].user;
+    // song = json_data[0].song;
 
+    // console.log("post_id: ", post_id);
+    // console.log("post_body: ", post_body);
+    // console.log("post_likes: ", post_likes);
+    // console.log("user: ", user);
+    // console.log("song: ", song);
+
+    console.log(`POST ID: ${json_data[0].post_id}`);
     console.log(json_data);
 
-
-    post_id = json_data[0].post_id;
-    post_body = json_data[0].post_body;
-    post_likes = json_data[0].post_likes; 
-    user = json_data[0].user;
-    song = json_data[0].song;
-
-
-    console.log("post_id: ", post_id);
-    console.log("post_body: ", post_body);
-    console.log("post_likes: ", post_likes);
-    console.log("user: ", user);
-    console.log("song: ", song);
-
-
-    newF(json_data);
-
+    console.log(typeof json_data);
+    displayPosts(json_data);
 
   }catch(error){
     console.log(error);
@@ -46,15 +41,22 @@ async function getPosts(){
   }
 }
 
+function displayPosts(posts) {
+  var parentSection = document.getElementById("posts");
+  console.log(`(displayPosts) POST ID: ${posts[0].post_id}`);
 
+  for (let i = 0; i < posts.length; i++) {
+    //console.log(posts[i].post_id);
+    //console.log(posts[i].post_body);
 
-function newF(json_data){
-
-  // console.log(json_data);
-
+    var postDiv = document.createElement("div");
+    var postBody = document.createElement("p");
+    postBody.innerHTML = posts[i].post_body;
+    postDiv.append(postBody);
+    parentSection.append(postDiv);
+  }
 }
 
 
 getPosts()
-
 
